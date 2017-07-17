nicolasstucki/dotty

Erase arguments of phantom type
This commit removes arguments and parameter of phantom type from all applications.
Arguments are evaluated before the application in case they have side effects.

```
def foo(i: Int, p: SomePhantom) = ???

foo(42, { println("hello"); getSomePhantom })
```

becomes

```
def foo(i: Int) = ???

val x$0 = 42
val x$1 = { println("hello"); getSomePhantom }
foo(x$0)
```

Note that now `def foo(i: Int)` and def `foo(i: Int, p: SomePhantom)` erase to the same signature.

Tests for this commit where added in `Extra tests for Phantoms 1`, they where back ported as the semantics and runtime is not affected.

compiler/src/dotty/tools/dotc/core/PhantomErasure.scala
@@ -5,10 +5,13 @@ import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.core.Symbols.defn
 import dotty.tools.dotc.core.Types.Type
 
-/** Phantom erasure erases (minimal erasure):
+/** Phantom erasure erases:
  *
- *  - Parameters/arguments are erased to BoxedUnit. The next step will remove the parameters
- *    from the method definitions and calls (implemented in branch implement-phantom-types-part-2).
+ *  - Parameters/arguments are erased removed from the function definition/call in `PhantomArgumentEval`.
+ *    If the evaluation of the phantom arguments may produce a side effect, these are evaluated and stored in
+ *    local `val`s and then the non phantoms are used in the Apply. Phantom `val`s are then erased to
+ *    `val ev$i: BoxedUnit = myPhantom` intended to be optimized away by local optimizations. `myPhantom` could be
+ *    a reference to a phantom parameter, a call to Phantom assume or a call to a method that returns a phantom.
  *  - Definitions of `def`, `val`, `lazy val` and `var` returning a phantom type to return a BoxedUnit. The next step
  *    is to erase the fields for phantom types (implemented in branch implement-phantom-types-part-3)
  *  - Calls to Phantom.assume become calls to BoxedUnit. Intended to be optimized away by local optimizations.
@@ -24,5 +27,8 @@ object PhantomErasure {
 
   /** Returns the default erased tree for a call to Phantom.assume */
   def erasedAssume(implicit ctx: Context): Tree = ref(defn.BoxedUnit_UNIT)
+
+  /** Returns the default erased tree for a phantom parameter ref */
+  def erasedParameterRef(implicit ctx: Context): Tree = ref(defn.BoxedUnit_UNIT)
 
 }

tests/run/phantom-erased-methods.scala
@@ -1,0 +1,33 @@
+import scala.runtime.BoxedUnit
+
+/* Run this test with
+ *   `run tests/run/xyz.scala -Xprint-diff-del -Xprint:arrayConstructors,phantomTermErasure,phantomTypeErasure,erasure`
+ * to see the the diffs after PhantomRefErasure, PhantomDeclErasure and Erasure.
+ */
+
+object Test {
+  import Boo._
+
+  def main(args: Array[String]): Unit = {
+    val foo = new Foo
+
+    // check that parameters have been removed from the functions
+    // (would fail with NoSuchMethodException if not erased)
+    foo.getClass.getDeclaredMethod("fun1")
+    foo.getClass.getDeclaredMethod("fun2", classOf[String])
+
+    assert(foo.getClass.getDeclaredMethod("fun3").getReturnType == classOf[BoxedUnit])
+  }
+}
+
+class Foo {
+  import Boo._
+  def fun1(b: BooAny): Unit = ()
+  def fun2(b: BooAny, s: String): Unit = ()
+  def fun3(): BooAny = boo
+}
+
+object Boo extends Phantom {
+  type BooAny = Boo.Any
+  def boo: BooAny = assume
+}

hamzaremmal/scala3

compiler/src/dotty/tools/dotc/core/PhantomErasure.scala
@@ -5,10 +5,13 @@ import dotty.tools.dotc.core.Contexts.Context
 import dotty.tools.dotc.core.Symbols.defn
 import dotty.tools.dotc.core.Types.Type
 
-/** Phantom erasure erases (minimal erasure):
+/** Phantom erasure erases:
  *
- *  - Parameters/arguments are erased to ErasedPhantom. The next step will remove the parameters
- *    from the method definitions and calls (implemented in branch implement-phantom-types-part-2).
+ *  - Parameters/arguments are erased removed from the function definition/call in `PhantomArgumentEval`.
+ *    If the evaluation of the phantom arguments may produce a side effect, these are evaluated and stored in
+ *    local `val`s and then the non phantoms are used in the Apply. Phantom `val`s are then erased to
+ *    `val ev$i: ErasedPhantom = myPhantom` intended to be optimized away by local optimizations. `myPhantom` could be
+ *    a reference to a phantom parameter, a call to Phantom assume or a call to a method that returns a phantom.
  *  - Definitions of `def`, `val`, `lazy val` and `var` returning a phantom type to return a ErasedPhantom. Where fields
  *    with ErasedPhantom type are not memoized (see transform/Memoize.scala).
  *  - Calls to Phantom.assume become calls to ErasedPhantom.UNIT. Intended to be optimized away by local optimizations.
@@ -20,5 +23,8 @@ object PhantomErasure {
 
   /** Returns the default erased tree for a call to Phantom.assume */
   def erasedAssume(implicit ctx: Context): Tree = ref(defn.ErasedPhantom_UNIT)
+
+  /** Returns the default erased tree for a phantom parameter ref */
+  def erasedParameterRef(implicit ctx: Context): Tree = ref(defn.ErasedPhantom_UNIT)
 
 }

tests/run/phantom-erased-methods.scala
@@ -1,0 +1,33 @@
+import dotty.runtime.ErasedPhantom
+
+/* Run this test with
+ *   `run tests/run/xyz.scala -Xprint-diff-del -Xprint:arrayConstructors,phantomTermErasure,phantomTypeErasure,erasure`
+ * to see the the diffs after PhantomRefErasure, PhantomDeclErasure and Erasure.
+ */
+
+object Test {
+  import Boo._
+
+  def main(args: Array[String]): Unit = {
+    val foo = new Foo
+
+    // check that parameters have been removed from the functions
+    // (would fail with NoSuchMethodException if not erased)
+    foo.getClass.getDeclaredMethod("fun1")
+    foo.getClass.getDeclaredMethod("fun2", classOf[String])
+
+    assert(foo.getClass.getDeclaredMethod("fun3").getReturnType == classOf[ErasedPhantom])
+  }
+}
+
+class Foo {
+  import Boo._
+  def fun1(b: BooAny): Unit = ()
+  def fun2(b: BooAny, s: String): Unit = ()
+  def fun3(): BooAny = boo
+}
+
+object Boo extends Phantom {
+  type BooAny = Boo.Any
+  def boo: BooAny = assume
+}