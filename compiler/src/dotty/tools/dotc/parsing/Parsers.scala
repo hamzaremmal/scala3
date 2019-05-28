rubenfiszel/dotty

Add syntactic sugar for polymorphic function types
Desugar the type
    [T_1, ..., T_M] -> (P_1, ..., P_N) => R
Into
    scala.PolyFunction { def apply[T_1, ..., T_M](x$1: P_1, ..., x$N: P_N): R }

compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -792,6 +792,8 @@ object Parsers {
           val tparams = typeParamClause(ParamOwner.TypeParam)
           if (in.token == ARROW)
             atPos(start, in.skipToken())(LambdaTypeTree(tparams, toplevelTyp()))
+          else if (isIdent && in.name.toString == "->")
+            atPos(start, in.skipToken())(PolyFunction(tparams, toplevelTyp()))
           else { accept(ARROW); typ() }
         }
         else infixType()

hamzaremmal/scala3

compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -924,6 +924,8 @@ object Parsers {
           val tparams = typeParamClause(ParamOwner.TypeParam)
           if (in.token == TLARROW)
             atSpan(start, in.skipToken())(LambdaTypeTree(tparams, toplevelTyp()))
+          else if (isIdent && in.name.toString == "->")
+            atSpan(start, in.skipToken())(PolyFunction(tparams, toplevelTyp()))
           else { accept(TLARROW); typ() }
         }
         else infixType()