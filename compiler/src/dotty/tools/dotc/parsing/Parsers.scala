rubenfiszel/dotty

Add syntactic sugar for polymorphic function values
Desugar the value
    [T_1, ..., T_M] -> (x_1: P_1, ..., x_N: P_N) => body
Into
    new scala.PolyFunction { def apply[T_1, ..., T_M](x_1: P_1, ..., x_N: P_N) = body }

compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -1194,6 +1194,11 @@ object Parsers {
         atPos(in.skipToken()) { Return(if (isExprIntro) expr() else EmptyTree, EmptyTree) }
       case FOR =>
         forExpr()
+      case LBRACKET =>
+        val start = in.offset
+        val tparams = typeParamClause(ParamOwner.TypeParam)
+        assert(isIdent && in.name.toString == "->", "Expected `->`")
+        atPos(start, in.skipToken())(PolyFunction(tparams, expr()))
       case _ =>
         expr1Rest(postfixExpr(), location)
     }

hamzaremmal/scala3

compiler/src/dotty/tools/dotc/parsing/Parsers.scala
@@ -1325,6 +1325,11 @@ object Parsers {
         atSpan(in.skipToken()) { Return(if (isExprIntro) expr() else EmptyTree, EmptyTree) }
       case FOR =>
         forExpr()
+      case LBRACKET =>
+        val start = in.offset
+        val tparams = typeParamClause(ParamOwner.TypeParam)
+        assert(isIdent && in.name.toString == "->", "Expected `->`")
+        atSpan(start, in.skipToken())(PolyFunction(tparams, expr()))
       case _ =>
         if (isIdent(nme.inline) && !in.inModifierPosition() && in.lookaheadIn(canStartExpressionTokens)) {
           val start = in.skipToken()