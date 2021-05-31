pikinier20/dotty

Run snippet compiler on static sites

scaladoc/src/dotty/tools/scaladoc/tasty/ScalaDocSupport.scala
@@ -9,8 +9,6 @@ import dotty.tools.scaladoc.tasty.comments.Comment
 trait ScaladocSupport { self: TastyParser =>
   import qctx.reflect._
 
-  private val snippetChecker: snippets.SnippetChecker = snippets.SnippetChecker()
-
   def parseCommentString(comment: String, sym: Symbol, pos: Option[Position]): Comment =
     val preparsed = comments.Preparser.preparse(comments.Cleaner.clean(comment))
 
@@ -29,9 +27,9 @@ trait ScaladocSupport { self: TastyParser =>
 
     val parser = commentSyntax match {
       case CommentSyntax.Wiki =>
-        comments.WikiCommentParser(comments.Repr(qctx)(sym), snippetChecker)
+        comments.WikiCommentParser(comments.Repr(qctx)(sym))
       case CommentSyntax.Markdown =>
-        comments.MarkdownCommentParser(comments.Repr(qctx)(sym), snippetChecker)
+        comments.MarkdownCommentParser(comments.Repr(qctx)(sym))
     }
     parser.parse(preparsed)
 

hamzaremmal/scala3

scaladoc/src/dotty/tools/scaladoc/tasty/ScalaDocSupport.scala
@@ -29,9 +29,9 @@ object ScaladocSupport:
 
     val parser = commentSyntax match {
       case CommentSyntax.Wiki =>
-        comments.WikiCommentParser(comments.Repr(quotes)(sym), snippetChecker)
+        comments.WikiCommentParser(comments.Repr(quotes)(sym))
       case CommentSyntax.Markdown =>
-        comments.MarkdownCommentParser(comments.Repr(quotes)(sym), snippetChecker)
+        comments.MarkdownCommentParser(comments.Repr(quotes)(sym))
     }
     parser.parse(preparsed)
 