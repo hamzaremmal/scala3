jchyb/dotty

Set correct comment syntax in stdlibs generated via scaladoc
Wiki syntax was set for scala 2.13 stdlib and markdown was set for scala
3 stdlib.

project/Build.scala
@@ -1836,6 +1836,10 @@ object ScaladocConfigs {
       .add(ExternalMappings(List(javaExternalMapping)))
       .add(DocRootContent(docRootFile.toString))
       .add(CommentSyntax("wiki"))
+      .add(CommentSyntaxOverrides(List(
+        s"${dottyLibRoot}=markdown",
+        s"${stdLibRoot}=wiki"
+      )))
       .add(VersionsDictionaryUrl("https://scala-lang.org/api/versions.json"))
       .add(DocumentSyntheticTypes(true))
       .add(SnippetCompiler(List(

hamzaremmal/scala3

project/Build.scala
@@ -1834,7 +1834,10 @@ object ScaladocConfigs {
       .add(Revision("main"))
       .add(ExternalMappings(List(javaExternalMapping)))
       .add(DocRootContent(docRootFile.toString))
-      .add(CommentSyntax(List("wiki")))
+      .add(CommentSyntax(List(
+        s"${dottyLibRoot}=markdown",
+        s"${stdLibRoot}=wiki"
+      )))
       .add(VersionsDictionaryUrl("https://scala-lang.org/api/versions.json"))
       .add(DocumentSyntheticTypes(true))
       .add(SnippetCompiler(List(