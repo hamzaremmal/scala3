mpollmeier/dotty

open up runBody|interpret|runUntilQuit, so we can call/override them from subclasses

compiler/src/dotty/tools/repl/ReplDriver.scala
@@ -138,7 +138,7 @@ class ReplDriver(settings: Array[String],
    *  observable outside of the CLI, for this reason, most helper methods are
    *  `protected final` to facilitate testing.
    */
-  final def runUntilQuit(initialState: State = initialState): State = {
+  def runUntilQuit(initialState: State = initialState): State = {
     val terminal = new JLineTerminal
 
     out.println(
@@ -182,7 +182,7 @@ class ReplDriver(settings: Array[String],
     interpret(parsed, quiet = true)
   }
 
-  private def runBody(body: => State): State = rendering.classLoader()(using rootCtx).asContext(withRedirectedOutput(body))
+  protected def runBody(body: => State): State = rendering.classLoader()(using rootCtx).asContext(withRedirectedOutput(body))
 
   // TODO: i5069
   final def bind(name: String, value: Any)(implicit state: State): State = state
@@ -248,7 +248,7 @@ class ReplDriver(settings: Array[String],
         .getOrElse(Nil)
   end completions
 
-  private def interpret(res: ParseResult, quiet: Boolean = false)(implicit state: State): State = {
+  protected def interpret(res: ParseResult, quiet: Boolean = false)(implicit state: State): State = {
     res match {
       case parsed: Parsed if parsed.trees.nonEmpty =>
         compile(parsed, state, quiet)

hamzaremmal/scala3

compiler/src/dotty/tools/repl/ReplDriver.scala
@@ -138,7 +138,7 @@ class ReplDriver(settings: Array[String],
    *  observable outside of the CLI, for this reason, most helper methods are
    *  `protected final` to facilitate testing.
    */
-  final def runUntilQuit(using initialState: State = initialState)(): State = {
+  def runUntilQuit(using initialState: State = initialState)(): State = {
     val terminal = new JLineTerminal
 
     out.println(
@@ -181,7 +181,7 @@ class ReplDriver(settings: Array[String],
     interpret(parsed, quiet = true)
   }
 
-  private def runBody(body: => State): State = rendering.classLoader()(using rootCtx).asContext(withRedirectedOutput(body))
+  protected def runBody(body: => State): State = rendering.classLoader()(using rootCtx).asContext(withRedirectedOutput(body))
 
   // TODO: i5069
   final def bind(name: String, value: Any)(using state: State): State = state
@@ -247,7 +247,7 @@ class ReplDriver(settings: Array[String],
         .getOrElse(Nil)
   end completions
 
-  private def interpret(res: ParseResult, quiet: Boolean = false)(using state: State): State = {
+  protected def interpret(res: ParseResult, quiet: Boolean = false)(using state: State): State = {
     res match {
       case parsed: Parsed if parsed.trees.nonEmpty =>
         compile(parsed, state, quiet)