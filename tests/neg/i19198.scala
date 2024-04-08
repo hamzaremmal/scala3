hamzaremmal/scala3

commit cd9c303c4aae711b50d7f272aa5885542c2248f7

Normalize only when needed

compiler/src/dotty/tools/dotc/typer/Synthesizer.scala
@@ -339,7 +339,7 @@ class Synthesizer(typer: Typer)(using @constructorOnly c: Context):
      *  or a TermRef to a singleton value. These are
      *  the base elements required to generate a mirror.
      */
-    def reduce(mirroredType: Type)(using Context): Either[String, MirrorSource] = mirroredType.normalized match
+    def reduce(mirroredType: Type)(using Context): Either[String, MirrorSource] = mirroredType match
       case tp: TypeRef =>
         val sym = tp.symbol
         if sym.isClass then // direct ref to a class, not an alias
@@ -379,6 +379,7 @@ class Synthesizer(typer: Typer)(using @constructorOnly c: Context):
                 // avoid type aliases for tuples
                 Right(MirrorSource.GenericTuple(types))
               case _ => reduce(tp.underlying)
+          case tp: MatchType => reduce(tp.normalized)
           case _ => reduce(tp.superType)
       case tp @ AndType(l, r) =>
         for

commit e0b45729b5a8f7ee7b09fe31c47d29b8d565d0f7

Fix infinite loop in Mirror synthesis of unreducible match type
This regressed in cd9c303c4aae711b50d7f272aa5885542c2248f7 (present in 3.4.0).

compiler/src/dotty/tools/dotc/typer/Synthesizer.scala
@@ -379,7 +379,7 @@ class Synthesizer(typer: Typer)(using @constructorOnly c: Context):
                 // avoid type aliases for tuples
                 Right(MirrorSource.GenericTuple(types))
               case _ => reduce(tp.underlying)
-          case tp: MatchType => reduce(tp.normalized)
+          case tp: MatchType => reduce(tp.tryNormalize.orElse(tp.superType))
           case _ => reduce(tp.superType)
       case tp @ AndType(l, r) =>
         for

tests/neg/i19198.scala
@@ -1,0 +1,9 @@
+import deriving.Mirror
+import compiletime.summonInline
+
+type DoesNotReduce[T] = T match
+  case String => Any
+
+class Foo
+@main def Test: Unit =
+  summonInline[Mirror.Of[DoesNotReduce[Option[Int]]]] // error